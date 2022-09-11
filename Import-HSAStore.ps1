c128dc497c67533a9ae076c68f51f7d030fcc6be
Graham Foral <93955617+gforal@users.noreply.github.com>
2022-09-10 23:19:00 -0500

    Add files via upload

---
+++ Import-HSAStore.ps1
@@ -0,0 +1,74 @@
+﻿<#
+.SYNOPSIS
+        Provide a method by which Microsoft Store apps can be loaded recursively without the use of JSON, XML, or any other type of configuration file.
+
+.DESCRIPTION
+
+        This script will traverse the path provided in the $HSAStore variable for telltale filetypes, then use the DISM-based built-in PowerShell modules to install them.
+
+.PARAMETER HSAStore
+        
+        Path to folder containing one, more, or all of the Store file types
+
+.PARAMETER Provision
+        
+        Flag, either True or False, about whether the app is provisioned for the computer as a provisioning AppX, or for users.
+
+.PARAMETER LogFile
+
+        Location to the path where the PSTranscript will be dumped. Default is the current directory.
+
+
+.NOTES
+
+    FileName: Import-HSAStore.ps1
+    Author:   Graham Foral
+    Created:  9/10/2022
+#>
+
+param(
+
+$HSAStore,
+$Provision,
+$LogFile="AddHSAStore" + (Get-Date -Format yyyy-MM-dd_HHmmss) + ".log"
+
+)
+
+$PackageDir = Get-ChildItem -Path $HSAStore -Directory -Recurse
+
+Start-Transcript -Path $LogFile
+ForEach($Package in $PackageDir) {
+    If((Get-ChildItem -Path $Package.FullName -File).count -lt 1) {
+        Write-Host "Warning: $($Package.Name) will be ignored because it contains no files. Subdirectories will be searched..." -ForegroundColor Yellow 
+        Continue
+    } else {
+    
+        Write-Host "Information: $($Package.FullName)" -ForegroundColor Green 
+
+        $HSA = Get-ChildItem -Path $Package.FullName -ErrorAction SilentlyContinue | Where-Object { ($_.Name -like "*.msix*") -or ($_.Name -like "*.*appxbundle") -or ($_.Name -like "*.*appx") -and ($_.Name -notlike "*Microsoft*") }
+        $Dependencies = Get-ChildItem -Path $Package.FullName -Filter "*Microsoft*" -ErrorAction SilentlyContinue
+        $License = Get-ChildItem -Path $Package.FullName -Filter "*.xml" -ErrorAction SilentlyContinue
+    
+        Write-Host "Package Content:"
+        Write-Host "- HSA: $($HSA.Name)"
+        Write-Host "- Dependencies: $($Dependencies.Name)"
+        Write-Host "- License: $($License.Name)"
+        Write-Host "`n"
+    
+        If($Provision -eq "True") {
+            If($License) {
+                Add-AppxProvisionedPackage -Online -PackagePath $HSA.FullName -LicensePath $License.FullName -DependencyPackagePath $Dependencies.FullName -LogLevel Errors
+            } else {
+                Add-AppxProvisionedPackage -Online -PackagePath $HSA.FullName -SkipLicense -DependencyPackagePath $Dependencies.FullName -LogLevel Errors
+            }
+         
+        } else {
+                If($Dependencies) {
+                    Add-AppxPackage -Path $HSA.FullName -DependencyPath $Dependencies.FullName
+                } else {
+                    Add-AppxPackage -Path $HSA.FullName
+                }
+        }
+    }
+   
+}
